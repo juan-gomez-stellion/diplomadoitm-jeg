65fa523ab16e855707e62da572d1ecf79a4f4ebc
juan-gomez-stellion <81586639+juan-gomez-stellion@users.noreply.github.com>
2024-11-20 01:57:04 +0000

    Subo tercera app

---
+++ docker/tercera-aplicacion/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import boto3
+
+app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+tabla = dynamodb.Table('tabla-juangomez')
+
+
+@app.route('/insert', methods=['POST'])
+def index():
+  data = request.json
+  item = {**data}
+
+  tabla.put_item(Item=item)
+
+  return 'Se guardó exitosamente'
+
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=5000)
