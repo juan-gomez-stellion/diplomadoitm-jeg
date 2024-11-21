06edab1a25f1364e94228b30b18bfdd112911062
juan-gomez-stellion <81586639+juan-gomez-stellion@users.noreply.github.com>
2024-11-21 01:21:16 +0000

    Creamos aplicacion para convertir texto a audio

---
+++ docker/cuarta-aplicacion/app.py
@@ -0,0 +1,32 @@
+import boto3
+from flask import Flask, request, jsonify, send_file
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/voice', methods=['POST'])
+def voice():
+  try:
+    data = request.json
+    text = data.get('text')
+    voice = "Mia"
+    output_format = "mp3"
+
+    if not text:
+      return jsonify({'error': 'No se recibió el texto'}), 400
+
+    polly = boto3.client('polly', region_name='us-east-1')
+
+    response = polly.synthesize_speech(Text=text,
+                                       OutputFormat=output_format,
+                                       VoiceId=voice)
+
+    audio_file = "output.mp3"
+    with open(audio_file, 'wb') as file:
+      file.write(response['AudioStream'].read())
+
+    return send_file(audio_file, mimetype='audio/mpeg')
+
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
