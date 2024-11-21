bef4da36bd762263b55895a9fcb1b8d831dac88a
juan-gomez-stellion <81586639+juan-gomez-stellion@users.noreply.github.com>
2024-11-21 00:14:11 +0000

    Se adiciona metodo para consultar elementos de dynamodb

--- docker/tercera-aplicacion/app.py
+++ docker/tercera-aplicacion/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import boto3
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tabla = dynamodb.Table('tabla-juangomez')
 
 
+#Metodo para insertar elementos en una tabla de dynamodb
 @app.route('/insert', methods=['POST'])
 def index():
   data = request.json
@@ -14,6 +15,21 @@ def index():
   tabla.put_item(Item=item)
 
   return 'Se guardó exitosamente'
+
+
+#Metodo para leer elementos de una tabla de dynamodb
+@app.route('/get/<id>', methods=['GET'])
+def get_item(id):
+  try:
+    response = tabla.get_item(Key={'id': id})
+
+    if 'Item' in response:
+      return jsonify(response['Item']), 200
+    else:
+      return jsonify({'error': 'id no existe'}), 404
+
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
 
 
 if __name__ == "__main__":
